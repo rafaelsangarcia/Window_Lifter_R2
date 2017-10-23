e84232c586e1f9d7b3755f976f6c75961c8f0599
Rodrigo Jonathan Mortera Peña <mortera.jonathan@gmail.com>
2017-10-23 14:09:17 -0500

    Add

--- WindowLifter/WindowLifter/Sources/portinit.c
+++
@@ -1,7 +0,0 @@
-#include "portinit.h"
-
-void WDOG_disable (void){
- WDOG->CNT=0xD928C520; /*Unlock watchdog*/
- WDOG->TOVAL=0x0000FFFF; /*Maximum timeout value*/
- WDOG->CS = 0x00002100; /*Disable watchdog*/
-}
